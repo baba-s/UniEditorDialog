15fa8f4885c99e9443e886bc4949d42f6c55099f
baba_s <shotababa1206@gmail.com>
2020-09-28 19:38:23 +0900

    first commit

---
+++ Editor/EditorDialog.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+namespace Kogane
+{
+	public sealed class EditorDialog
+	{
+		private readonly string m_title;
+
+		public EditorDialog( string title )
+		{
+			m_title = title;
+		}
+
+		public void OpenOk( string message )
+		{
+			EditorUtility.DisplayDialog
+			(
+				title: m_title,
+				message: message,
+				ok: "OK"
+			);
+		}
+
+		public bool OpenYesNo( string message )
+		{
+			return EditorUtility.DisplayDialog
+			(
+				title: m_title,
+				message: message,
+				ok: "はい",
+				cancel: "いいえ"
+			);
+		}
+	}
+}
